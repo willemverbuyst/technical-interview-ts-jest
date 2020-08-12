aed8540815493d47e9cc33ca27bc6442efe438a3
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-12 06:39:07 +0200

    Test splitCharacters

--- string.test.ts
+++ string.test.ts
@@ -1,5 +1,28 @@
-import { splitString } from './string';
+import { splitString, splitCharacters } from './string';
 
 test('Split "Vanilla JavaScript" results in ["Vanilla", "JavaScript"]', () => {
   expect(splitString('Vanilla JavaScript')).toEqual(['Vanilla', 'JavaScript']);
+});
+
+test("splitCharacters 'Vanilla JavaScript' returns ['V', 'a', 'n', 'i', 'l','l', 'a', ' ', 'J', 'a', 'v', 'a', 'S', 'c', 'r', 'i', 'p', 't']", () => {
+  expect(splitCharacters('Vanilla JavaScript')).toEqual([
+    'V',
+    'a',
+    'n',
+    'i',
+    'l',
+    'l',
+    'a',
+    ' ',
+    'J',
+    'a',
+    'v',
+    'a',
+    'S',
+    'c',
+    'r',
+    'i',
+    'p',
+    't',
+  ]);
 });
--- string.ts
+++ string.ts
@@ -1,2 +1,4 @@
 // Split string
 export const splitString = (str: string): string[] => str.split(' ');
+
+export const splitCharacters = (str: string): string[] => str.split('');
