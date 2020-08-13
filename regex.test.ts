a8c95527db3ca399350949066533acf26f864864
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-13 18:50:53 +0200

    Test replaceWordInString

--- regex.test.ts
+++ regex.test.ts
@@ -1,4 +1,4 @@
-import { changeToSpinalCase } from './regex';
+import { changeToSpinalCase, replaceWordInString } from './regex';
 
 describe('changeToSpinalCase', () => {
   test('is defined', () => {
@@ -9,5 +9,37 @@ describe('changeToSpinalCase', () => {
     expect(changeToSpinalCase("Let's_get_things_done")).toBe(
       "let's-get-things-done"
     );
+  });
+});
+
+describe('replaceWordInString', () => {
+  test('A quick brown fox jumped over the lazy dog, jumped, leaped', () => {
+    expect(
+      replaceWordInString(
+        'A quick brown fox jumped over the lazy dog',
+        'jumped',
+        'leaped'
+      )
+    ).toBe('A quick brown fox leaped over the lazy dog');
+  });
+
+  test('Today I will learn regular expressions, Today,Tomorrow', () => {
+    expect(
+      replaceWordInString(
+        'Today I will learn regular expressions',
+        'Today',
+        'Tomorrow'
+      )
+    ).toBe('Tomorrow I will learn regular expressions');
+  });
+
+  test('Today I will learn regular expressions, Today,Tomorrow', () => {
+    expect(
+      replaceWordInString(
+        'Today I will learn regular expressions',
+        'Today',
+        'Tomorrow'
+      )
+    ).not.toBe('Today I will learn regular expressions');
   });
 });
--- regex.ts
+++ regex.ts
@@ -4,3 +4,15 @@ export const changeToSpinalCase = (str: string): string =>
     .split(/\s|_|(?=[A-Z])/g)
     .join('-')
     .toLowerCase();
+
+// // Replace word in string, preserve capital
+export const replaceWordInString = (
+  str: string,
+  before: string,
+  after: string
+): string => {
+  const capitals = /[A-Z]/;
+  return capitals.test(before[0])
+    ? str.replace(before, after[0].toUpperCase() + after.slice(1))
+    : str.replace(before, after);
+};
