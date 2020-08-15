849290409bc3cf8516dc32ea78b98c8a9fd9a22a
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-15 10:18:10 +0200

    Test getUniqueNumbers, solution-2

--- arrays.test.ts
+++ arrays.test.ts
@@ -1,7 +1,20 @@
-import { getUniqueNumbers_1 } from './arrays';
+import { getUniqueNumbers_1, getUniqueElements_2 } from './arrays';
 
 describe('getUniqueNumbers', () => {
   describe('getUniqueNumbers_1', () => {
+    test('[1, 2, 3, 4, 7] and [2, 3, 4, 5, 6]', () => {
+      expect(getUniqueNumbers_1([1, 2, 3, 4, 7], [2, 3, 4, 5, 6])).toEqual([
+        1,
+        7,
+        5,
+        6,
+      ]);
+    });
+    test('[2, 3, 4] and [2, 3, 4]', () => {
+      expect(getUniqueNumbers_1([2, 3, 4], [2, 3, 4])).toEqual([]);
+    });
+  });
+  describe('getUniqueNumbers_2', () => {
     test('[1, 2, 3, 4, 7] and [2, 3, 4, 5, 6]', () => {
       expect(getUniqueNumbers_1([1, 2, 3, 4, 7], [2, 3, 4, 5, 6])).toEqual([
         1,
--- arrays.ts
+++ arrays.ts
@@ -4,3 +4,9 @@ export const getUniqueNumbers_1 = (arr1: number[], arr2: number[]): number[] =>
   [...arr1, ...arr2].filter(
     (element) => arr1.indexOf(element) === -1 || arr2.indexOf(element) === -1
   );
+
+// Solution 2
+export const getUniqueElements_2 = (arr1: number[], arr2: number[]): number[] =>
+  [...arr1, ...arr2].filter(
+    (element) => !arr1.includes(element) || !arr2.includes(element)
+  );
